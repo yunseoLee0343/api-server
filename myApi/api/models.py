ba21a9433b089b9108ba42c89c17cf571527f599
Yunseo Lee <lys139011@gmail.com>
2024-04-12 12:55:58 +0900

    feat : Add models

--- myApi/api/models.py
+++ myApi/api/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
-
-
-
+class Product(models.Model):
+    new_product = models.BooleanField(default=False)
+    product_name = models.CharField(max_length=100)
+    cate_name = models.CharField(max_length=100)
+    content = models.TextField()
+    calories = models.IntegerField()
+    sugars = models.IntegerField()
+    protein = models.IntegerField()
+    caffeine = models.IntegerField()
+    fat = models.IntegerField()
+    sodium = models.IntegerField()
