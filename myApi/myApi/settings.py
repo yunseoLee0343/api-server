36370c435df03d48213ecfc1e67289c0d9820215
Yunseo Lee <lys139011@gmail.com>
2024-04-12 13:16:25 +0900

    feat : Add mysql

--- myApi/myApi/settings.py
+++ myApi/myApi/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newdinker',
+        'USER': 'root',
+        'PASSWORD': 'jede1004',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
